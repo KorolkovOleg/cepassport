b7b99fd4e383f1628e42a73188888000aea15060
oleg <helkorolkov@gmail.com>
2025-05-29 11:45:14 +0300

    init

---
+++ src/main/java/com/ekzonpractice/cepassport/CepassportApplication.java
@@ -0,0 +1,13 @@
+package com.ekzonpractice.cepassport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepassportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepassportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekzonpractice/cepassport/CepassportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekzonpractice.cepassport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepassportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
